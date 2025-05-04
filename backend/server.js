10f22beae93f9510f7f8d85bae91641f9b438756
Sammysk12 <samarthkalshetti43@gmail.com>
2025-05-04 23:36:30 +0530

    Finalfinal changes

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ await connectClodinary();
 
 //5.
 //Allow multiple origins
-const allowedOrigin = ["*"];
+const allowedOrigin = ["http://localhost:5173", "https://green-cart-grocery-app-rkt9.vercel.app"];
 
 //4.
 //Middleware configuration

172a5709ed8c7ea9731d38d56ac97ab393505d0b
Sammysk12 <samarthkalshetti43@gmail.com>
2025-05-06 13:06:22 +0530

    cors changes

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -50,11 +50,13 @@ const MyOrders = () => {
             >
               <div className="flex items-center mb-4 md:mb-0">
                 <div className="bg-primary/10 p-4 rounded-lg">
+                  {item.product.image &&
                   <img
                     src={item.product.image[0]}
                     alt=""
                     className="w-16 h-16"
                   />
+                  } 
                 </div>
                 <div className="ml-4">
                   <h2 className="text-xl font-medium text-gray-800">
